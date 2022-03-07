e25e3fc6befdbd89b472e56c753a92435492f4d9
Santiago Inostroza <santiagoinostroza2@gmail.com>
2022-03-07 02:10:54 -0300

    ajustes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,13 +14,27 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(BrandSeeder::class);
-        $this->call(SupplierSeeder::class);
-        $this->call(CategorySeeder::class);
-        $this->call(ProductSeeder::class);
-        $this->call(RoleSeeder::class);
-        $this->call(UserSeeder::class);
-        $this->call(CustomerSeeder::class);
-        $this->call(PurchaseSeeder::class);
+        $tipo=2;
+
+        if($tipo == 1){  //desarrollo
+
+            $this->call(BrandSeeder::class);
+            $this->call(SupplierSeeder::class);
+            $this->call(CategorySeeder::class);
+            $this->call(ProductSeeder::class);
+            $this->call(RoleSeeder::class);
+            $this->call(UserSeeder::class);
+            $this->call(CustomerSeeder::class);
+            $this->call(PurchaseSeeder::class);
+        
+        }
+        if($tipo == 2){  //produccion
+
+            $this->call(BrandSeeder::class);
+            $this->call(CategorySeeder::class);
+            $this->call(RoleSeeder::class);
+        
+        }
+        
     }
 }
